c261ab495dbd64b85bf63ba9839f51515e6aed6c
Joe Biellik <joebiellik@gmail.com>
2018-11-29 00:47:44 +0000

    Improve status output

--- Modules/Status.cs
+++ Modules/Status.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Threading.Tasks;
-using CommandLine;
+﻿using CommandLine;
 using JetBrains.Annotations;
 using NFive.PluginManager.Extensions;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using NFive.SDK.Plugins.Configuration;
 using Console = Colorful.Console;
 
 namespace NFive.PluginManager.Modules
@@ -20,46 +21,134 @@ namespace NFive.PluginManager.Modules
 		internal async Task<int> Main()
 		{
 			var cd = Path.GetFullPath(Environment.CurrentDirectory);
-			Console.WriteLine($"Current directory: {cd}", Color.Green);
+			Console.WriteLine($"Current directory: {cd}");
 
-			var bin = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
-			Console.WriteLine($"nfpm.exe: {Relative(bin, cd)}", Color.Green);
+			Console.WriteLine();
 			
-			string server = null;
-			try
-			{
-				server = Relative(PathManager.FindServer(), cd);
-			}
-			catch (Exception) { }
+			// NFPM
 
-			Console.WriteLine($"Server: {server ?? "NOT FOUND"}");
-			
-			string resource = null;
-			try
-			{
-				resource = Relative(PathManager.FindResource(), cd);
-			}
-			catch (Exception) { }
+			var nfpm = Process.GetCurrentProcess().MainModule;
 
-			Console.WriteLine($"Resource: {resource ?? "NOT FOUND"}");
-			
-			bool current = false;
-			try
-			{
-				current = PathManager.IsResource();
-			}
-			catch (Exception) { }
+			Console.WriteLine("NFPM:");
+			Console.WriteLine($"-- Path: {Relative(Path.GetDirectoryName(nfpm.FileName), cd)}");
+			Console.WriteLine($"-- Binary: {Relative(nfpm.FileName, cd)}");
+			Console.WriteLine($"-- Version: {nfpm.FileVersionInfo.FileVersion}");
 
-			Console.WriteLine($"IsResource: {current}");
+			Console.WriteLine();
+
+			// FIVEM
+
+			var server = FindServer();
+
+			Console.Write("FIVEM:");
+
+			if (string.IsNullOrWhiteSpace(server))
+			{
+				Console.WriteLine(" NOT FOUND");
+			}
+			else
+			{
+				Console.WriteLine();
+				Console.WriteLine($"-- Path: {Relative(server, cd)}");
+
+				var binary = Path.Combine(server, "FXServer.exe");
+				Console.Write("-- Binary: ");
+				Console.WriteLine(File.Exists(binary) ? Relative(binary, cd) : "MISSING");
+
+				var version = GetServerVersion(server);
+				Console.Write("-- Version: ");
+				Console.WriteLine(!string.IsNullOrWhiteSpace(version) ? version : "UNKNOWN");
+
+				var config = Path.Combine(server, "server.cfg");
+				Console.Write("-- Config: ");
+				Console.WriteLine(File.Exists(config) ? Relative(config, cd) : "MISSING");
+			}
+
+			Console.WriteLine();
+
+			// NFIVE
+
+			var nfive = FindResource();
+
+			Console.Write("NFIVE:");
+
+			if (string.IsNullOrWhiteSpace(nfive))
+			{
+				Console.WriteLine(" NOT FOUND");
+			}
+			else
+			{
+				Console.WriteLine();
+				Console.WriteLine($"-- Path: {Relative(nfive, cd)}");
+
+				var version = AssemblyName.GetAssemblyName(Path.Combine(nfive, "NFive.Server.net.dll")).Version;
+				Console.WriteLine($"-- Version: {version}");
+
+				var definition = Path.Combine(nfive, ConfigurationManager.DefinitionFile);
+				Console.Write("-- Definition: ");
+				Console.WriteLine(File.Exists(definition) ? Relative(definition, cd) : "MISSING");
+
+				var @lock = Path.Combine(nfive, ConfigurationManager.LockFile);
+				Console.Write("-- Lock: ");
+				Console.WriteLine(File.Exists(@lock) ? Relative(@lock, cd) : "MISSING");
+			}
 
 			return await Task.FromResult(0);
+		}
+
+		private string FindServer()
+		{
+			try
+			{
+				return PathManager.FindServer();
+			}
+			catch (Exception)
+			{
+				return string.Empty;
+			}
+		}
+
+		private string FindResource()
+		{
+			try
+			{
+				return PathManager.FindResource();
+			}
+			catch (Exception)
+			{
+				return string.Empty;
+			}
+		}
+
+		private string GetServerVersion(string serverPath)
+		{
+			try
+			{
+				return File.ReadAllText(Path.Combine(serverPath, "version"));
+			}
+			catch (Exception)
+			{
+				return string.Empty;
+			}
 		}
 
 		private string Relative(string path, string cd)
 		{
 			var res = path.TrimStart(cd);
 
-			return string.IsNullOrEmpty(res) ? @".\" : res;
+			if (string.IsNullOrEmpty(res))
+			{
+				return @".\";
+			}
+			else
+			{
+				if (res == path)
+				{
+					return res;
+				}
+
+				return "." + res;
+			}
 		}
 	}
 }
