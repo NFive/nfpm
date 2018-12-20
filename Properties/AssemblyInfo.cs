a7acb807525bf542ea26d1d0c00b2e85c8e2486a
Joe Biellik <joebiellik@gmail.com>
2018-12-20 01:38:21 +0000

    Improve build versioning

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
+#pragma warning disable CS7035
 [assembly: AssemblyTitle("NFive Plugin Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -11,6 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b0a726a9-7252-4116-95b1-dcaeb7349f07")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0-alpha")]
+[assembly: AssemblyInformationalVersion("0.1 Alpha")]
