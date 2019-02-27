2097b4cc6e548a6ea7c54b81f4974b96627d93fc
Joe Biellik <joebiellik@gmail.com>
2019-02-27 23:30:07 +0000

    Impliment update command

--- Modules/Update.cs
+++ Modules/Update.cs
@@ -1,5 +1,12 @@
 using CommandLine;
+using NFive.PluginManager.Configuration;
+using NFive.PluginManager.Utilities;
+using NFive.SDK.Plugins;
+using NFive.SDK.Plugins.Configuration;
+using System;
+using System.IO;
 using System.Threading.Tasks;
+using DefinitionGraph = NFive.PluginManager.Models.DefinitionGraph;
 
 namespace NFive.PluginManager.Modules
 {
@@ -9,6 +16,32 @@ namespace NFive.PluginManager.Modules
 	[Verb("update", HelpText = "Update installed NFive plugins.")]
 	internal class Update
 	{
-		internal async Task<int> Main() => await Task.FromResult(0);
+		internal async Task<int> Main()
+		{
+			Plugin definition;
+
+			try
+			{
+				Environment.CurrentDirectory = PathManager.FindResource();
+
+				definition = Plugin.Load(ConfigurationManager.DefinitionFile);
+			}
+			catch (FileNotFoundException ex)
+			{
+				Console.WriteLine(ex.Message);
+				Console.WriteLine("Use `nfpm setup` to setup NFive in this directory");
+
+				return 1;
+			}
+
+			var graph = new DefinitionGraph();
+			await graph.Build(definition);
+			await graph.Apply(definition);
+			graph.Save();
+
+			if (PathManager.IsResource()) ResourceGenerator.Serialize(graph).Save();
+
+			return await Task.FromResult(0);
+		}
 	}
 }
