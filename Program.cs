4404e2ce5f7342b96a36efe6595d68e34d311d00
Joe Biellik <joebiellik@gmail.com>
2019-02-27 20:53:11 +0000

    Reorder modules

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 using CommandLine;
 using NFive.PluginManager.Modules;
+using NFive.PluginManager.Utilities;
 using System;
 using System.Threading.Tasks;
-using NFive.PluginManager.Utilities;
 
 namespace NFive.PluginManager
 {
@@ -61,38 +61,38 @@ namespace NFive.PluginManager
 				return await Parser
 					.Default
 					.ParseArguments<
-						Setup,
-						Search,
-						List,
-						Install,
-						Remove,
-						Update,
-						Outdated,
-						SelfUpdate,
-						Start,
-						Scaffold,
-						Rcon,
-						Status,
-						Migrate,
 						CleanCache,
-						Pack
+						Install,
+						List,
+						Migrate,
+						Outdated,
+						Pack,
+						Rcon,
+						Remove,
+						Scaffold,
+						Search,
+						SelfUpdate,
+						Setup,
+						Start,
+						Status,
+						Update
 					>(args)
 					.MapResult(
-						(Setup s) => s.Main(),
-						(Search s) => s.Main(),
-						(List l) => l.Main(),
-						(Install i) => i.Main(),
-						(Remove r) => r.Main(),
-						(Update u) => u.Main(),
-						(Outdated o) => o.Main(),
-						(SelfUpdate s) => s.Main(),
-						(Start s) => s.Main(),
-						(Scaffold s) => s.Main(),
-						(Rcon r) => r.Main(),
-						(Status s) => s.Main(),
-						(Migrate s) => s.Main(),
-						(CleanCache c) => c.Main(),
-						(Pack p) => p.Main(),
+						(CleanCache m) => m.Main(),
+						(Install m) => m.Main(),
+						(List m) => m.Main(),
+						(Migrate m) => m.Main(),
+						(Outdated m) => m.Main(),
+						(Pack m) => m.Main(),
+						(Rcon m) => m.Main(),
+						(Remove m) => m.Main(),
+						(Scaffold m) => m.Main(),
+						(Search m) => m.Main(),
+						(SelfUpdate m) => m.Main(),
+						(Setup m) => m.Main(),
+						(Start m) => m.Main(),
+						(Status m) => m.Main(),
+						(Update m) => m.Main(),
 						e => Task.FromResult(1)
 					);
 			}
