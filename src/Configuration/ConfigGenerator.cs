8deb158882089ff50d83d98cb75203dcf4d41176
Joe Biellik <joebiellik@gmail.com>
2021-07-14 00:11:16 +0100

    Update with FiveM changes

--- src/Configuration/ConfigGenerator.cs
+++ src/Configuration/ConfigGenerator.cs
@@ -14,21 +14,21 @@ namespace NFive.PluginManager.Configuration
 
 		public string Hostname { get; set; } = "NFive";
 
+		public List<string> Tags { get; set; } = new List<string> { "NFive" };
+
+		public string Locale { get; set; } = "en-US";
+
+		public ushort MaxPlayers { get; set; } = 48;
+
+		public bool OneSync { get; set; } = true;
+
 		public string LicenseKey { get; set; } = null;
 
 		public string SteamKey { get; set; } = "none";
 
 		public string RconPassword { get; set; } = null;
 
-		public ushort MaxPlayers { get; set; } = 32;
-
-		public string Locale { get; set; } = "en-US";
-
-		public bool OneSync { get; set; } = true;
-
 		public bool ScriptHookAllowed { get; set; } = false;
-
-		public List<string> Tags { get; set; } = new List<string> { "NFive" };
 
 		[SuppressMessage("ReSharper", "StringLiteralTypo")]
 		public void Serialize(string path)
@@ -38,19 +38,20 @@ namespace NFive.PluginManager.Configuration
 			output.AppendLine($"endpoint_add_tcp \"{this.Endpoint}\"");
 			output.AppendLine($"endpoint_add_udp \"{this.Endpoint}\"");
 			output.AppendLine();
-			output.AppendLine($"sv_hostname \"{this.Hostname}\"");
+			output.AppendLine($"sets sv_hostname \"{this.Hostname}\"");
+			output.AppendLine($"sets sv_projectName \"{this.Hostname}\"");
+			output.AppendLine($"sets sv_projectDesc \"{this.Hostname}\"");
 			output.AppendLine($"sets tags \"{string.Join(", ", this.Tags)}\"");
-			output.AppendLine($"sv_maxclients {this.MaxPlayers}");
-			output.AppendLine($"set locale \"{this.Locale}\"");
-			output.AppendLine($"set onesync_enabled {this.OneSync.ToString().ToLowerInvariant()}");
+			output.AppendLine($"sets locale \"{this.Locale}\"");
 			output.AppendLine();
-			output.AppendLine($"sv_licensekey \"{this.LicenseKey}\"");
+			output.AppendLine($"set onesync {this.OneSync.ToString().ToLowerInvariant()}");
+			output.AppendLine($"set sv_maxclients {this.MaxPlayers}");
+			output.AppendLine($"set sv_licensekey \"{this.LicenseKey}\"");
 			output.AppendLine($"set steam_webApiKey \"{this.SteamKey}\"");
-			output.AppendLine($"rcon_password \"{this.RconPassword}\"");
-			output.AppendLine();
-			output.AppendLine("sv_endpointPrivacy true");
-			output.AppendLine("sv_enhancedHostSupport true");
-			output.AppendLine($"sv_scriptHookAllowed {this.ScriptHookAllowed.ToString().ToLowerInvariant()}");
+			output.AppendLine($"set rcon_password \"{this.RconPassword}\"");
+			output.AppendLine($"set sv_scriptHookAllowed {this.ScriptHookAllowed.ToString().ToLowerInvariant()}");
+			//output.AppendLine("set sv_endpointPrivacy true");
+			//output.AppendLine("set sv_enhancedHostSupport true");
 			output.AppendLine();
 			output.AppendLine("start nfive");
 
